8a076b5069abfaddf9dd87e7d46f0e9539965d65
nftraja <nftraja1@gmail.com>
2026-03-15 14:45:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,37 @@
+const CACHE_NAME = "ladyglamour-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js",
+"/images/logo.png"
+];
+
 self.addEventListener("install", function(event) {
-console.log("Service Worker Installed");
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(function(cache) {
+return cache.addAll(urlsToCache);
+})
+);
+
 });
 
 self.addEventListener("fetch", function(event) {
+
+event.respondWith(
+caches.match(event.request)
+.then(function(response) {
+
+if(response){
+return response;
+}
+
+return fetch(event.request);
+
+})
+);
+
 });
